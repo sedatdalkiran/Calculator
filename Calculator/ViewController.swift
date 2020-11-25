e15b30da87f70d6b003901b0f3ba7b1271412ed3
Sedat <sedatdlkrn46@gmail.com>
2020-11-25 14:49:56 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Sedat Dalkıran on 25.11.2020.
+//  Copyright © 2020 Sedat Dalkıran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
